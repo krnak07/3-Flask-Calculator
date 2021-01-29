de86ed05d848d4fe492189efbe3080c737d33343
Kiran A K <34866636+krnak07@users.noreply.github.com>
2021-01-29 11:18:08 +0530

    first commit

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 from flask import Flask, render_template, request
-from flask_cors import CORS, cross_origin
 app = Flask(__name__)
-cors = CORS(app)
 
 
 @app.route('/')
-@cross_origin()
 def index():
     return render_template('index.html')
 
 
 @app.route('/eval')
-@cross_origin()
 def evaluate():
     return str(eval(request.args.get('exp'))), 200
 
