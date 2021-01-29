2d81540b05986992c65ed310de99048af759e1f8
Kiran A K <kiran.ak@corp.knowledgelens.com>
2021-01-29 11:19:36 +0530

    first commit

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+from flask_cors import CORS, cross_origin
 app = Flask(__name__)
+cors = CORS(app)
 
 
 @app.route('/')
+@cross_origin()
 def index():
     return render_template('index.html')
+
+
+@app.route('/eval')
+@cross_origin()
+def evaluate():
+    return str(eval(request.args.get('exp'))), 200
 
 
 if __name__ == '__main__':
